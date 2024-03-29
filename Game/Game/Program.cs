de0ffdae9f85339d90de3c0acc7dcdff783696b7
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-03-29 22:50:17 +0300

    implement player class, implement map rendering

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,2 +1,6 @@
-﻿var a = new Map("/home/sarapulov-vasilii/work/Game/Game/Game/map.txt");
-a.DisplayMap();
+﻿using Game;
+var player = new Player();
+var map = new Map("/home/sarapulov-vasilii/work/Game/Game/Game/map.txt", player);
+map.DisplayMap(player);
+player.MoveRight(map);
+player.MoveUp(map);
