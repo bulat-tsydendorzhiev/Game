483466cde31ab58d207cfd2eeae66786febbfba4
Sarapulov Vasiliy <sarapulov.vas@mail.ru>
2024-03-29 18:11:02 +0300

    add map class

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var a = new Map("/home/sarapulov-vasilii/work/Game/Game/Game/map.txt");
+a.DisplayMap();
